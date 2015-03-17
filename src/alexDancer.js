69ce33d441e46d75ec042120fd6383f325a992bd
Sharky <kaydenalthen@gmail.com>
2015-03-17 17:27:33 -0500

    minor cleanup

--- src/alexDancer.js
+++ src/alexDancer.js
@@ -1,6 +1,5 @@
 var AlexDancer = function (top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  // this.$node = $('<img src="alexhead.png" class="thirdDancer"></img>');
   this.$node.addClass("alexDancer");
 };
 
@@ -13,7 +12,7 @@ AlexDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   var sideStep = function() { this.$node.animate({ left: "-=10"}, 500 ); };
   // var tops = (Math.random()*20);
-   var lefts = (Math.random()*500);
+  var lefts = (Math.random()*500);
   var slowDown = function() { this.$node.animate({top: "-=200", left: "-=15"}, 1000, sideStep.call(this) ); };
   this.$node.animate({ top: "+=" + lefts, left: "+=15" }, 1200, slowDown.call(this) );
   // $('dancer').animate({ top: "+=45", left: "+=15" }, 1200
